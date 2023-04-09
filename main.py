e64570be89a6c2d5fb0fda4a431801342b480cd0
Joalison Matheus <joalison.ferreira@academico.ifpb.edu.br>
2023-04-08 21:36:38 -0300

    created news functions and file resultado.json

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# verificar a existência de correlação entre a idade da mãe quando
-# adolescente (entre 13 e 19 anos) e o peso do recém nascido
-
-# quebrar o algoritmo em dois de 2021 até 2014 e de 2103 até 2005
-
 import os
 from estatistica.correlacao import correlacao
+import json
 
 def anoArquivo(nomeArquivo):
   caminhoArquivo = nomeArquivo.split('/')
-  anoDoArquivo = caminhoArquivo[2].split('.')
-  anoDoArquivo = int(anoDoArquivo[0])
+  anoDoArquivo = caminhoArquivo[2].split('.')[0]
 
-  return anoDoArquivo
+  return int(anoDoArquivo)
+
+def escreverNoArquivo(dicionario):
+  with open("resultado.json", "w") as file:
+    text = json.dumps(dicionario)
+    file.write(text)
+    file.close()
+
+def lerArquivoDict(caminho):
+  with open(caminho, "r") as file:
+    dictString = file.read()
+
+  return json.loads(dictString)
 
 def antesDe2013(nomeArquivo) -> bool:
   if(anoArquivo(nomeArquivo) > 2013):
@@ -19,41 +26,47 @@ def antesDe2013(nomeArquivo) -> bool:
 
   return False
 
-def escreverNoArquivo(nomeArquivo, correlacao):
-  with open("resultado.txt", "a") as file:
-    text = str(anoArquivo(nomeArquivo)) + " --> " + str(correlacao) + "\n\r"
-    file.write(text)
-    file.close()
-
-def correlacaoIdadeMaexPesoFilho(nomeArquivo, idadeMae, pesoFilho):
-  idade = []
-  peso = []
+def pesoTotalFilho(nomeArquivo, idadeMae, pesoFilho):
+  pesos = {
+    "13": [0, 0], "14": [0, 0], "15": [0, 0],
+    "16": [0, 0], "17": [0, 0], "18": [0, 0],
+    "19": [0, 0]
+  }
 
   with open(nomeArquivo, "r") as file:
     rows = file.readlines()
 
   for i in range(len(rows)):
-    idadeMin = int(rows[i][idadeMae['posicaoInicial']:idadeMae['posicaoFinal']]) >= 13
-    idadeMax = int(rows[i][idadeMae['posicaoInicial']:idadeMae['posicaoFinal']]) <= 19
+    idade = rows[i][idadeMae['posicaoInicial']:idadeMae['posicaoFinal']]
+    peso = float(rows[i][pesoFilho['posicaoInicial']:pesoFilho['posicaoFinal']])
+
+    idadeMin = int(idade) >= 13
+    idadeMax = int(idade) <= 19
+
     if(idadeMin and idadeMax):
-      idade.append(float(rows[i][idadeMae['posicaoInicial']:idadeMae['posicaoFinal']]))
-      peso.append(float(rows[i][pesoFilho['posicaoInicial']:pesoFilho['posicaoFinal']]))
+        if (idade in pesos):
+          pesos[idade][0] += peso
+          pesos[idade][1] += 1
 
-  resultado = correlacao(idade, peso)
+  dictAntigo = lerArquivoDict("resultado.json")
 
-  escreverNoArquivo(nomeArquivo, resultado)
+  for i in range(13, 20):
+    dictAntigo[str(i)][0] += pesos[str(i)][0]
+    dictAntigo[str(i)][1] += pesos[str(i)][1]
+
+  escreverNoArquivo(dictAntigo)
 
 def analiseArquivo(nomeArquivo, antesDe2013):
   if (antesDe2013): # de 2021 à 2014
     posicaoIdadeMae = {'posicaoInicial': 74, 'posicaoFinal': 76}
     posicaoPesoFilho = {'posicaoInicial': 503, 'posicaoFinal': 507}
-    correlacaoIdadeMaexPesoFilho(nomeArquivo, posicaoIdadeMae, posicaoPesoFilho)
+    pesoTotalFilho(nomeArquivo, posicaoIdadeMae, posicaoPesoFilho)
   else:# de 2013 à 2006
     posicaoIdadeMae = {'posicaoInicial': 88, 'posicaoFinal': 90}
     posicaoPesoFilho = {'posicaoInicial': 462, 'posicaoFinal': 466}
-    correlacaoIdadeMaexPesoFilho(nomeArquivo, posicaoIdadeMae, posicaoPesoFilho)
+    pesoTotalFilho(nomeArquivo, posicaoIdadeMae, posicaoPesoFilho)
 
-def processoFilho(numeroProcesso, maxProcessosParalelos, pastas, pastaRaiz):
+def processoFilho(numeroProcesso, maxProcessosParalelos, pastas, pastaRaiz) -> dict[int, list[int]]:
   print("Processo %i criado!"%(numeroProcesso+1))
 
   for i in range(numeroProcesso, len(pastas), maxProcessosParalelos):
@@ -63,9 +76,31 @@ def processoFilho(numeroProcesso, maxProcessosParalelos, pastas, pastaRaiz):
       print("Processo %i --> %s"%(numeroProcesso+1, nomeArquivo))
       analiseArquivo(nomeArquivo, antesDe2013(nomeArquivo))
 
-pastaRaiz = "database"
-maxProcessosParalelos = 2
+def medias(pesosDict):
+  medias = {"13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0}
+  for i in range(13, 20):
+    medias[str(i)] = pesosDict[str(i)][0] / pesosDict[str(i)][1]
+
+  return medias
+
+def calcularCorrelacao(mediasDict):
+  idades = [13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0]
+  mediasPesos = []
+  for i in range(13, 20):
+    mediasPesos.append(float(mediasDict[str(i)]))
+
+  return correlacao(idades, mediasPesos)
+
+
+pastaRaiz = input("Digite o nome da pasta raíz: ")
+maxProcessosParalelos = int(input("Digite o número de processos: "))
 pastas = os.listdir(pastaRaiz)
+
+escreverNoArquivo({
+  "13": [0, 0], "14": [0, 0], "15": [0, 0],
+  "16": [0, 0], "17": [0, 0], "18": [0, 0],
+  "19": [0, 0]
+})
 
 for numeroProcesso in range(0, maxProcessosParalelos):
   if(os.fork() == 0):
@@ -73,9 +108,17 @@ for numeroProcesso in range(0, maxProcessosParalelos):
     exit(0)
 
 for numero in range(0, maxProcessosParalelos):
-    os.wait()
+  os.wait()
 
-print("\n====================================================")
-print("Análise concluída com sucesso!!!")
-print("Abrir o arquivo resultado.txt para ver os resultados")
-print("====================================================")
+dictPesos = lerArquivoDict("resultado.json")
+mediasPesos = medias(dictPesos)
+correlacaoIdadePeso = calcularCorrelacao(mediasPesos)
+
+print("\n=====================================================")
+print("Análise concluída com sucesso!!!\n")
+for i in range(13, 20):
+  print("Idade %i; Peso total: %.2fg; Peso médio: %.2fg; N: %i"%(
+    i, dictPesos[str(i)][0], mediasPesos[str(i)], dictPesos[str(i)][1]
+  ))
+print("\nCorrelação: %.2f"%(correlacaoIdadePeso))
+print("\n=====================================================")
